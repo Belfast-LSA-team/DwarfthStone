5889d4ac459fb4541bb96362c58b796e6f6d5e1e
Anton Kolomiiets <resdenia@gmail.com>
2021-03-16 23:28:13 +0200

    predifine server add react to the project

---
+++ client/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.tsx",
+    output: {
+        path: path.join(__dirname, "/public"),
+        filename: "bundle.js",
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".css", ".scss", ".html"],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        historyApiFallback: true,
+        compress: true,
+        port: 3010,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+};
