118ecda32cbf572a0bb73672dc3d19461d71ccac
Anton Kolomiiets <resdenia@gmail.com>
2021-03-18 21:11:01 +0200

    added boilerplate for the page

--- client/src/router/GameRouter/index.tsx
+++ client/src/router/GameRouter/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Homepage from "../../pages/homepage/Homepage";
+import Homepage from "../../pages/homepage";
 
 export const GameRouter = (): JSX.Element => (
     <Router>
