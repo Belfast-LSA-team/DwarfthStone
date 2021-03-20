1e8abd7d77b1bbd8625e18958103bf2f0c158cfd
Anton Kolomiiets <resdenia@gmail.com>
2021-03-20 10:19:54 +0200

    spinner component

---
+++ client/src/components/spinner/Spinner.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./spinner.css";
+
+// build here https://loading.io/
+// example <Spinner color={"blue"} />
+
+type SpinnerProps = {
+    color: "blue" | "orange";
+};
+
+export const Spinner: React.FC<SpinnerProps> = ({ color }) => {
+    let class_name = "spinner";
+    class_name += ` ${color}`;
+
+    return (
+        <svg
+            className={class_name}
+            viewBox="0 0 100 100"
+            preserveAspectRatio="xMidYMid"
+        >
+            <path d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" stroke="none">
+                <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    dur="1.2048192771084336s"
+                    repeatCount="indefinite"
+                    keyTimes="0;1"
+                    values="0 50 51;360 50 51"
+                ></animateTransform>
+            </path>
+        </svg>
+    );
+};
