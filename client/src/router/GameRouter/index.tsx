32c486cba551653ad9f1da006447dbda18a9291b
Anton Kolomiiets <resdenia@gmail.com>
2021-03-21 23:00:38 +0200

    added 404 and 500 pages added 404 to router + upload cards boilerplate

--- client/src/router/GameRouter/index.tsx
+++ client/src/router/GameRouter/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Homepage from "../../pages/homepage";
+import Page404 from "../../pages/404";
 
 export const GameRouter = (): JSX.Element => (
     <Router>
         <Switch>
             <Route exact path="/">
                 <Homepage />
+            </Route>
+            <Route path="*">
+                <Page404 message="Страница не найдена" />
             </Route>
         </Switch>
     </Router>
