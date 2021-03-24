f50a25142fa24d35052f329e2da01c99136e5821
Sergey Gutskal <gutskalss@Sergeys-MacBook-Air.local>
2021-03-24 21:24:53 +0300

    Add modal component

---
+++ client/src/components/modal/Modal.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import classnames from "classnames";
+import { toggleElement } from "../../utils/index";
+
+import "./modal.css";
+
+type ModalProps = {
+    className?: string;
+    children: React.ReactNode;
+};
+
+function closeModalOnOutClick(event: React.MouseEvent) {
+    if ((event.target as HTMLElement).classList.contains("modal")) {
+        toggleElement(".modal");
+    }
+}
+
+export const Modal = (props: ModalProps) => {
+    const { className, children } = props;
+    const classNames = classnames("modal", className);
+
+    return (
+        <div
+            className={classNames}
+            onClick={(e: React.MouseEvent) => closeModalOnOutClick(e)}
+        >
+            {children}
+        </div>
+    );
+};
