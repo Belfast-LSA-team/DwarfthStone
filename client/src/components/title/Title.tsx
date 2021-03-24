3da7d5147d7fffae50b845761d08527231982a2a
Sergey Gutskal <gutskalss@Sergeys-MacBook-Air.local>
2021-03-24 11:57:09 +0300

    Add title component

---
+++ client/src/components/title/Title.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import classnames from "classnames";
+
+import "./title.css";
+
+type TitleProps = {
+    text: string;
+    className?: string;
+    level: 1 | 2 | 3 | 4 | 5 | 6;
+};
+
+export const Title = (props: TitleProps) => {
+    const { text, className, level } = props;
+    const classNames = classnames("title", `h${level}`, className);
+
+    switch (level) {
+        case 1:
+            return <h1 className={classNames}>{text}</h1>;
+        case 2:
+            return <h2 className={classNames}>{text}</h2>;
+        case 3:
+            return <h3 className={classNames}>{text}</h3>;
+        case 4:
+            return <h4 className={classNames}>{text}</h4>;
+        case 5:
+            return <h5 className={classNames}>{text}</h5>;
+        case 6:
+            return <h6 className={classNames}>{text}</h6>;
+        default:
+            return null;
+    }
+};
