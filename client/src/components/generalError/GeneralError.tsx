9df2e43e955fe15c1ba9a2f05539139095a611b7
Anton Kolomiiets <resdenia@gmail.com>
2021-03-29 23:13:31 +0300

    added generalError

--- client/src/components/generalError/GeneralError.tsx
+++ client/src/components/generalError/GeneralError.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { Fragment, useState, useEffect } from "react";
+
+import "./generalError.css";
 
 type GeneralErrorProps = {
     message: string;
@@ -11,6 +13,11 @@ export const GeneralError = ({
     errorStatus,
     errorCode,
 }: GeneralErrorProps): JSX.Element => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 7000);
+    }, [loading]);
     const messageHandler = () => {
         const messageContent = [];
         if (errorStatus) {
@@ -23,36 +30,45 @@ export const GeneralError = ({
         return messageContent.join(" ");
     };
     return (
-        <div className="generalError_content">
-            <div className="generalError_content_message">
-                <p>{messageHandler()}</p>
-            </div>
-            <div className="generalError_content_close">
-                <div className="inner_close">
-                    <svg
-                        width="9"
-                        height="9"
-                        viewBox="0 0 9 9"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <line
-                            x1="7.7953"
-                            y1="1.35358"
-                            x2="0.724228"
-                            y2="8.42465"
-                            stroke="#F9F6F6"
-                        />
-                        <line
-                            x1="8.08834"
-                            y1="8.35358"
-                            x2="1.01727"
-                            y2="1.28252"
-                            stroke="#F9F6F6"
-                        />
-                    </svg>
+        <Fragment>
+            {loading && (
+                <div className="generalError">
+                    <div className="generalError_content">
+                        <div className="generalError_content_message">
+                            <p>{messageHandler()}</p>
+                        </div>
+                        <div className="generalError_content_close">
+                            <div
+                                onClick={() => setLoading(false)}
+                                className="inner_close"
+                            >
+                                <svg
+                                    width="9"
+                                    height="9"
+                                    viewBox="0 0 9 9"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <line
+                                        x1="7.7953"
+                                        y1="1.35358"
+                                        x2="0.724228"
+                                        y2="8.42465"
+                                        stroke="#F9F6F6"
+                                    />
+                                    <line
+                                        x1="8.08834"
+                                        y1="8.35358"
+                                        x2="1.01727"
+                                        y2="1.28252"
+                                        stroke="#F9F6F6"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            )}
+        </Fragment>
     );
 };
---
+++ client/src/components/generalError/index.tsx
@@ -0,0 +1,1 @@
+export { GeneralError as default } from "./GeneralError";
