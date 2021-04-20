02d54beab17b8f35a3a024fd439795fb87d5aa17
LStaynov <elstaino1@gmail.com>
2021-04-20 14:44:42 +0300

    SW skeleton

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -45,6 +46,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./client/public/index.html",
+        }),
+        new WorkboxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true,
+            swDest: "serviceWorker.js",
+            navigateFallback: "index.html",
         }),
     ],
 };
